3d64e2a177bb92607d4778064113658fd847dad8
Nikhil Yekhe <yendenikhil@gmail.com>
2019-02-08 17:41:45 +0100

    Create maven project
    Generated the project from start.spring.io.

---
+++ src/main/java/dev/ny/pastebin/pastebincore/PastebinCoreApplication.java
@@ -0,0 +1,14 @@
+package dev.ny.pastebin.pastebincore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastebinCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastebinCoreApplication.class, args);
+	}
+
+}
+
