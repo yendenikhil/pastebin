51aa7efaec62336d17fc2a5c93fd99b18d3b46a1
Nikhil Yekhe <nikhil.yekhe@ing.com>
2019-09-05 16:46:07 +0200

    some clean up

--- src/main/java/dev/ny/pastebin/bin/BinApi.java
+++ src/main/java/dev/ny/pastebin/bin/BinApi.java
@@ -2,10 +2,16 @@ package dev.ny.pastebin.bin;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -16,14 +22,17 @@ public class BinApi {
     private static final String ZERO = "0";
     private static final String TEN = "10";
     private static final String ID = "id";
-    @Autowired
-    private BinRepository repo;
+    private final BinRepository repo;
+
+    public BinApi(BinRepository repo) {
+        this.repo = repo;
+    }
 
 
     @GetMapping
     public List<Bin> getSearchResults(@RequestParam(value = "q", defaultValue = "") final String title,
-                                          @RequestParam(value = "pageNum", defaultValue = ZERO) final int pageNum,
-                                          @RequestParam(value = "pageSize", defaultValue = TEN) final int pageSize) {
+                                      @RequestParam(value = "pageNum", defaultValue = ZERO) final int pageNum,
+                                      @RequestParam(value = "pageSize", defaultValue = TEN) final int pageSize) {
         log.debug("GET: /: query={}, pageNum:{}, pageSize:{}", title, pageNum, pageSize);
         return repo.findByTitleIgnoreCaseContaining(title,
                 PageRequest.of(pageNum, pageSize, Sort.by(Sort.Direction.DESC, ID)));
@@ -44,7 +53,7 @@ public class BinApi {
         });
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}")
     public void deleteBinWithId(@PathVariable("id") final Long id) {
         log.debug("DELETE: /{}: deleting bin if exists", id);
         repo.deleteById(id);
